7e8cb83ceaf291a246959ecc1693afe2de9e1db1
Dirag Biswas <hey@diragb.dev>
2025-10-23 01:39:46 +0530

    feat: get `unifiedBlocklist` within `initialize`

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,42 +1,176 @@
-import { get, KEYS } from "@/utils/localStorage"
+// Packages:
+import { KEYS, get, set } from '@/utils/localStorage'
+import { sha256 } from '@/utils/sha'
 
 // Typescript:
 type Pair<T, U> = [T, U]
 
 // Constants:
 const DEFAULT_BLOCKLIST_PREFERENCES: string[] = []
-const mutedUsernames = new Set('ChillamChilli')
 
 // Functions:
-const setDefaultBlocklistPreferences = () => {
+const getBlocklistPreferences = () => get<string[]>({
+  key: KEYS.blocklistPreferences,
+  defaultValue: DEFAULT_BLOCKLIST_PREFERENCES,
+  onNull: () => set({
+    key: KEYS.blocklistPreferences,
+    value: DEFAULT_BLOCKLIST_PREFERENCES,
+  }),
+  processor: preferences => {
+    const parsedPreferences = JSON.parse(preferences)
+    if (typeof parsedPreferences === 'object' && Array.isArray(parsedPreferences)) {
+      return parsedPreferences as string[]
+    } else {
+      return null
+    }
+  }
+})
+
+const setBlocklistPreferences = (blocklistIDs: string[]) => set({
+  key: KEYS.blocklistPreferences,
+  value: blocklistIDs,
+})
+
+const getBlocklistHashes = (blocklistIDs: string[]) => {
+  const blocklistHashes: Map<string, string | null> = new Map()
+  for (const blocklistID of blocklistIDs) {
+    const blocklistHash = get<string | null>({
+      key: KEYS.blocklistHash(blocklistID)
+    })
+    blocklistHashes.set(blocklistID, blocklistHash.value)
+  }
+
+  return blocklistHashes
+}
+
+const setBlocklistHash = async (blocklistID: string, blocklist: string[]) => {
   try {
-    localStorage.setItem('slop-blocker-preferences', JSON.stringify(DEFAULT_BLOCKLIST_PREFERENCES))
+    const blocklistHash = await sha256(blocklist.join(','))
+    set({
+      key: KEYS.blocklistHash(blocklistID),
+      value: blocklistHash,
+    })
   } catch (error) {
-    console.error('Error setting default slop-blocker-preferences to localStorage:', error)
+    console.warn(`Encountered an error while attempting to generate blocklist hash for key "${KEYS.blocklistHash(blocklistID)}":`, error)
   }
 }
 
-const getBlocklistPreferences = () => {
-  return get<string[]>(
-    KEYS.blocklistPreferences,
-    DEFAULT_BLOCKLIST_PREFERENCES,
-    setDefaultBlocklistPreferences,
-    preferences => {
-      const parsedPreferences = JSON.parse(preferences)
-      if (typeof parsedPreferences === 'object' && Array.isArray(parsedPreferences)) {
-        return parsedPreferences as string[]
-      } else {
-        return null
-      }
+const getBlocklist = (blocklistID: string) => get<string[]>({
+  key: KEYS.blocklist(blocklistID),
+  defaultValue: [],
+  onNull: () => set({
+    key: KEYS.blocklist(blocklistID),
+    value: [],
+  }),
+  processor: blocklistUsernames => {
+    const parsedBlocklistUsernames = JSON.parse(blocklistUsernames)
+    if (typeof parsedBlocklistUsernames === 'object' && Array.isArray(parsedBlocklistUsernames)) {
+      return parsedBlocklistUsernames as string[]
+    } else {
+      return null
     }
-  )
+  }
+})
+
+const setBlocklist = (blocklistID: string, blocklist: string[]) => set({
+  key: KEYS.blocklist(blocklistID),
+  value: blocklist,
+})
+
+
+const fetchBlocklistHashFromRemote = async (blocklistID: string): Promise<string | null> => {
+  try {
+    const response = await fetch(`https://slop-blocker.diragb.dev/blocklist-hashes/${blocklistID}`)
+    const blocklistHash = await response.text()
+    return blocklistHash
+  } catch (error) {
+    console.warn(`Encountered an error while attempting to fetch the blocklist hash from remote for "${blocklistID}":`, error)
+    return null
+  }
 }
 
-const fetchBlocklistFromRemote = async (blocklistID: string) => {
-  // const response = await fetch(`https://slop-blocker.diragb.dev/blocklists/${blocklistID}`)
-  // const blocklist = (await response.text()).split(',')
-  // return blocklist
-  return ['ChillamChilli', 'elonmusk']
+const fetchBlocklistFromRemote = async (blocklistID: string): Promise<string[]> => {
+  try {
+    const response = await fetch(`https://slop-blocker.diragb.dev/blocklists/${blocklistID}`)
+    const blocklist = (await response.text()).split(',')
+    return blocklist
+  } catch (error) {
+    console.warn(`Encountered an error while attempting to fetch the blocklist from remote for "${blocklistID}":`, error)
+    return []
+  }
+}
+
+const getOutOfSyncBlocklists = async (blocklistIDs: string[]): Promise<string[]> => {
+  const blocklistHashes = getBlocklistHashes(blocklistIDs)
+  const fetchedBlocklistHashes = new Map<string, string | null>()
+  const blocklistsOutOfSyncWithRemote: string[] = []
+
+  const blocklistHashPromises = new Map<string, Promise<string | null>>()
+  for (const blocklistID of blocklistIDs) {
+    blocklistHashPromises.set(
+      blocklistID,
+      fetchBlocklistHashFromRemote(blocklistID)
+    )
+  }
+
+  const resolvedBlocklistHashes = await Promise.all(blocklistHashPromises.values())
+  for (let i = 0; i < resolvedBlocklistHashes.length; i++) {
+    fetchedBlocklistHashes.set(blocklistIDs[i], resolvedBlocklistHashes[i])
+  }
+
+  for (const blocklistID of blocklistIDs) {
+    if (
+      blocklistHashes.get(blocklistID) === null ||
+      fetchedBlocklistHashes.get(blocklistID) === null ||
+      blocklistHashes.get(blocklistID) !== fetchedBlocklistHashes.get(blocklistID)
+    ) {
+      blocklistsOutOfSyncWithRemote.push(blocklistID)
+    }
+  }
+
+  return blocklistsOutOfSyncWithRemote
+}
+
+const getUnifiedBlocklist = async (blocklistIDs: string[], isFreshInstall: boolean) => {
+  const blocklistsToFetch = isFreshInstall ? blocklistIDs : await getOutOfSyncBlocklists(blocklistIDs)
+  const cachedBlocklistIDs: string[] = isFreshInstall ? [] : blocklistIDs.filter(blocklistID => !blocklistsToFetch.includes(blocklistID))
+  const fetchedBlocklists = new Map<string, string[]>()
+  const cachedBlocklists = new Map<string, string[]>()
+
+  const blocklistPromises = new Map<string, Promise<string[]>>()
+  for (const blocklistID of blocklistsToFetch) {
+    blocklistPromises.set(
+      blocklistID,
+      fetchBlocklistFromRemote(blocklistID)
+    )
+  }
+
+  const resolvedBlocklists = await Promise.all(blocklistPromises.values())
+  for (let i = 0; i < resolvedBlocklists.length; i++) {
+    const blocklistID = blocklistsToFetch[i]
+    fetchedBlocklists.set(blocklistID, resolvedBlocklists[i])
+    const blocklist = resolvedBlocklists[i].flat().sort((a, b) => String(a).localeCompare(String(b)))
+    setBlocklist(blocklistID, blocklist)
+    setBlocklistHash(blocklistID, blocklist)
+  }
+  const flatResolvedBlocklists = resolvedBlocklists.flat()
+
+  const flatCachedBlocklists: string[] = []
+  for (const blocklistID of cachedBlocklistIDs) {
+    const { value: blocklist } = getBlocklist(blocklistID)
+    cachedBlocklists.set(blocklistID, blocklist)
+    flatCachedBlocklists.push(...blocklist)
+  }
+
+  const unifiedBlocklists = [...flatResolvedBlocklists, ...flatCachedBlocklists]
+  return new Set(unifiedBlocklists.sort((blocklistUsernameA, blocklistUsernameB) => String(blocklistUsernameA).localeCompare(String(blocklistUsernameB))))
+}
+
+const initialize = async () => {
+  const { value: blocklistPreferences, wasNull: wasBlocklistPreferencesNull } = getBlocklistPreferences()
+  const isFreshInstall = wasBlocklistPreferencesNull === 'yes' || wasBlocklistPreferencesNull === 'indeterminate'
+  const unifiedBlocklist = await getUnifiedBlocklist(blocklistPreferences, isFreshInstall)
+  // TODO: use the unifiedBlocklist later on..
 }
 
 const scanTweets = (): Array<Pair<string, Element>> => {
@@ -52,21 +186,10 @@ const scanTweets = (): Array<Pair<string, Element>> => {
   return returnable
 }
 
-const initialize = async () => {
-  const blocklistPreferences = getBlocklistPreferences()
-  const blocklistPromises: Promise<string[]>[] = []
-  for (const blocklistPreference of blocklistPreferences) {
-    blocklistPromises.push(fetchBlocklistFromRemote(blocklistPreference))
-  }
-
-  const allBlocklists = await Promise.all(blocklistPromises)
-  const mutedUsernames = new Set(allBlocklists.flat())
-}
-
 const main = () => {
   const scannedTweetTuples = scanTweets()
   if (scannedTweetTuples.length > 0) {
-
+    // TODO:
   }
 }
 
--- utils/localStorage.ts
+++ utils/localStorage.ts
@@ -1,22 +1,88 @@
+// Constants:
+const SIGNATURE = 'slop-blocker-'
+
 // Exports:
 export const KEYS = {
   blocklistPreferences: 'blocklist-preferences',
-}
+  blocklist: (blocklistID: string) => blocklistID,
+  blocklistHash: (blocklistID: string) => `${blocklistID}-hash`,
+} as const
 
-export const get = <T = null>(key: string, defaultValue?: T, onNull?: () => void, processor?: (unprocessed: string) => T | null): T => {
+export const get = <T = null>({
+  key,
+  defaultValue,
+  onNull,
+  processor,
+}: {
+  key: string
+  defaultValue?: T
+  onNull?: () => void
+  processor?: (unprocessed: string) => T | null
+}): {
+  value: T,
+  wasNull: 'yes' | 'no' | 'indeterminate'
+} => {
   try {
-    key = 'slop-blocker-' + key
+    key = SIGNATURE + key
     const value = localStorage.getItem(key)
-    const returnable = value === null ? (defaultValue ?? null as T) : value as T
+    const returnable = value === null ? {
+      value: (defaultValue ?? null as T),
+      wasNull: 'yes' as const,
+    } : {
+      value: value as T,
+      wasNull: 'no' as const,
+    }
+    
     if (value === null && onNull !== undefined) onNull()
     if (processor !== undefined && value !== null) {
       const parsedValue = processor(value) as T | null
       if (parsedValue === null && onNull !== undefined) onNull()
-      return parsedValue === null ? (defaultValue ?? null as T) : parsedValue
+      return parsedValue === null ? {
+        value: (defaultValue ?? null as T),
+        wasNull: 'yes'
+      } : {
+        value: parsedValue,
+        wasNull: 'no',
+      }
     }
     else return returnable
   } catch (error) {
-    console.error(`Encountered an error while attempting to access key ${key} in LocalStorage`)
-    return (defaultValue ?? null as T)
+    console.error(`Encountered an error while attempting to access key "${key}" in LocalStorage:`, error)
+    return {
+      value: (defaultValue ?? null as T),
+      wasNull: 'indeterminate',
+    }
+  }
+}
+
+export const set = <T>({
+  key,
+  value,
+  override,
+}: {
+  key: string
+  value: T
+  override?: boolean
+}): boolean => {
+  try {
+    key = SIGNATURE + key
+    let stringifiedValue: string | null = null
+
+    try {
+      stringifiedValue = JSON.stringify(value)
+    } catch (error) {
+      console.error(`Encountered an error while attempting to stringify value to store to key "${key}" in LocalStorage:`, error)
+    }
+
+    if (stringifiedValue !== null) {
+      localStorage.setItem(key, stringifiedValue)
+      return true
+    } else if (stringifiedValue === null && override) {
+      localStorage.removeItem(key)
+      return true
+    } else return false
+  } catch (error) {
+    console.error(`Encountered an error while attempting to set key "${key}" in LocalStorage:`, error)
+    return false
   }
 }
